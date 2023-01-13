22d4dfc66af6489b48d13dcf9834a9259458871e
mehadhamad <zlotytozli@gmail.com>
2023-01-13 12:31:01 -0500

    Changed comment

--- backend/classes/office.php
+++ backend/classes/office.php
@@ -5,7 +5,7 @@ class Office {
     function __construct($key, $name){
         $this->$key = $key;
         $this->$name = $name;
-        $query = Manager::$database->db->prepare("SELECT * FROM Dates WHERE NAME LIKE '$name'"); # Todo: adjust query 2 day ago
+        $query = Manager::$database->db->prepare("SELECT * FROM Dates WHERE NAME LIKE '$name'"); # Todo: adjust query 2 day ago.
         $query->execute();
         $this->$dates = $query->fetch(PDO::FETCH_ASSOC);
     }
